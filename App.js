eaeeae6969a5019b6891713c8cb59e3ce6bd9cac
Nancy Som <nancysom2712@gmail.com>
2026-03-14 21:42:17 +0530

    dependencies loaded

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import ReactDOM from 'react-dom/client';
 const App = () =>{
     return <div>Hi</div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<App/>);
 
